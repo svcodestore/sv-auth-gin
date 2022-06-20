d4dfe7834cff8b3a8086a5670917ef7566f6e94c
yangwb1123 <1832227346@qq.com>
2022-06-20 11:28:06 +0800

    feat(utils): add tree random func `RandomInt`

--- utils/rand.go
+++ utils/rand.go
@@ -1,7 +1,9 @@
 package utils
 
 import (
+	cryptoRand "crypto/rand"
 	"github.com/thanhpk/randstr"
+	"math/big"
 	"math/rand"
 	"time"
 )
@@ -10,6 +12,12 @@ func RandRange(min, max int) (n int) {
 	rand.Seed(time.Now().Unix())
 	n = rand.Intn(max-min) + min
 	return
+}
+
+func RandomInt(max int64) (n int64) {
+	r, _ := cryptoRand.Int(cryptoRand.Reader, big.NewInt(max))
+
+	return r.Int64()
 }
 
 func GenerateClientId() string {
